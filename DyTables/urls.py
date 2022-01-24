b9d5d6a3217e6f2af18ab45e67e3d4b2d36f7a13
lalit <lalitpatidar211999@gmail.com>
2022-01-25 01:19:26 +0530

    In  Production  Mode setUp

--- DyTables/urls.py
+++ DyTables/urls.py
@@ -2,8 +2,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+import TMS
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('TMS.urls')),
 ]
+
+handler404 = TMS.views.handler404
+handler500 = TMS.views.handler500

b9950a5463a4f4a9e4ca2c370b1980d2d60a985f
lalit21-logico <59661128+lalit21-logico@users.noreply.github.com>
2022-01-27 13:51:26 +0530

    Update admin.py

--- TMS/admin.py
+++ TMS/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 from TMS.models import *
 # Register your models here.
+
+admin.site.register(UserTables)
+admin.site.register(SavedFilter)
+admin.site.register(AuditHistory)
